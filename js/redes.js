fdfb7c3237a9420c731e22fde55c5dc1814ebf84
edwardytb <104452175+edwardytb@users.noreply.github.com>
2022-11-20 17:50:44 -0600

    Update redes.js

--- js/redes.js
+++ js/redes.js
@@ -3,3 +3,7 @@ var cargando
 setTimeout(function(){alert("Porfavor espera un momento"); }, 100);
 
 setTimeout(function(){ alert("cargando.... esto puede demorar"); }, 1100);
+
+console.log("cargando...")
+console.log("buscando informacion..")
+console.log("porfavor espera")
