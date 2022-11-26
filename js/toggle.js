8f28aafacabcc5b57fc6fccdf384e121eb3f917d
edwardytb <104452175+edwardytb@users.noreply.github.com>
2022-11-25 22:37:49 -0600

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -2,9 +2,15 @@ const body = document.querySelector('body');
 const toggle = document.getElementById('toggle-box');
 const circle = document.getElementById('circle');
 const main = document.getElementById('main-');
+const card = document.getElementById('card');
+const text = document.getElementById('text-1');
+const imagen = document.getElementById('imagen')
 toggle.onclick = function(){
     toggle.classList.toggle('active')
     body.classList.toggle('active')
     circle.classList.toggle('active')
     main.classList.toggle('active')
+    card.classList.toggle('active')
+    text.classList.toggle('active')
+    imagen.classList.toggle('active')
 };
