a058d6ca833f18e041427fab502a2029628e4c02
edwardytb <104452175+edwardytb@users.noreply.github.com>
2022-12-04 18:30:15 -0600

    Delete ayuda directory

--- ayuda/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body');
-const toggle = document.getElementById('toggle-box');
-const circle = document.getElementById('circle');
-toggle.onclick = function(){
-    toggle.classList.toggle('active')
-    body.classList.toggle('active')
-    circle.classList.toggle('active')
-};
