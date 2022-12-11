33afdc9c539b0ce946c3c8851aba6bcec68fa26d
edwardytb <104452175+edwardytb@users.noreply.github.com>
2022-12-11 17:24:30 -0600

    Update toggle.js

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -6,20 +6,6 @@ const card = document.getElementById('card');
 const text = document.getElementById('text-1');
 const imagen = document.getElementById('imagen')
 toggle.onclick = function(){
-    //1
-    if (typeof(Storage) !== "undefined") {
-        //2
-        let cookies1 =["toggle", "body", "main", "circle"]
-        //localStorage.setItem("cookies1", cookies1);
-        localStorage.setItem("cookies1", JSON.stringify(cookies1));
-        //leer elementos almacenados
-        let cookiesalmacenadas = JSON.parse(localStorage.getItem("cookies1"));
-        console.log(cookiesalmacenadas);
-        //console.log(localStorage.getItem("cookies1"));
-    
-    } else {
-        alert("LocalStorage no soportado en este navegador");
-    }
     //toggle
     toggle.classList.toggle('active')
     body.classList.toggle('active')
