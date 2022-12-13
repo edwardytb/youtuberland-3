d7c68d26ab710fd26694ff42050518b19f7c0daa
edwardytb <104452175+edwardytb@users.noreply.github.com>
2022-12-13 13:04:30 -0600

    Delete assets directory

--- assets/js/arbol.js
+++
@@ -1,9 +0,0 @@
-$('.container').on('mouseover', function(){
-    $('.bauble').addClass('light');
-    $('.star').addClass('star-light');
-}
-)
-  
-  $('.container').on('mouseout', function(){
-    $('.bauble').removeClass('light');
-  })
